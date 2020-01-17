b71783def6a9ecc4c6675eeaebc5ad0aa664d3a2
Yacinesaf <yacinesaf96@gmail.com>
2020-01-17 14:17:56 -0500

    finished Sign in componenet

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -6,7 +6,8 @@ import { Avatar, Typography, TextField, FormControlLabel, Checkbox, Button, Grid
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 import InputAdornment from '@material-ui/core/InputAdornment';
-
+import Snackbar from '@material-ui/core/Snackbar';
+import Alert from '@material-ui/lab/Alert';
 
 class SingIn extends Component {
   constructor() {
@@ -16,6 +17,10 @@ class SingIn extends Component {
       email: 'test@gmail.com',
       password: 'testooo',
       checkBoxChecked: false,
+      passwordErrorMsg: 'Password needs to be at least 6 characters',
+      emailErrorMsg: 'Invalid Email',
+      validEmail: 'qwerty@gmail.com',
+      validPassword: '1234567',
       centered: {
         display: 'flex',
         flexDirection: 'column',
@@ -46,7 +51,7 @@ class SingIn extends Component {
     return obj
   }
 
-  validateEmail = (email) => {
+  isEmailValid = (email) => {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
   }
@@ -63,9 +68,22 @@ class SingIn extends Component {
     });
   }
 
+  isPasswordValid = (password) => {
+    return password.length >= 6
+  }
+
+  isFormValid = () => {
+    return this.isEmailValid(this.state.email) && this.state.checkBoxChecked && this.isPasswordValid(this.state.password)
+  }
+
+  showSnackBar = () => {
+    return this.state.password === this.state.validPassword && this.state.email === this.state.validEmail && this.state.checkBoxChecked
+  }
 
   render() {
-    const { centered, showingPassword, checkBoxChecked } = this.state;
+    const { centered, showingPassword, checkBoxChecked, password } = this.state;
+    let isFormValid = this.isFormValid();
+    let showSnackBar = this.showSnackBar()
     return (
       <div style={{ background: 'linear-gradient(to right, #1d4350, #a43931)', height: "100vh" }}>
 
@@ -87,8 +105,8 @@ class SingIn extends Component {
               <form style={{ width: '100%' }}>
 
                 <TextField
-                  error={this.validateEmail(this.state.email) ? false : true}
-                  helperText={this.validateEmail(this.state.email) ? '' : 'Incorrect entry'}
+                  error={!this.isEmailValid(this.state.email)}
+                  helperText={this.isEmailValid(this.state.email) ? '' : this.state.emailErrorMsg}
                   onChange={this.handleChangeEventEmail}
                   id="outlined-basic"
                   style={this.space('m', 'y', 24)}
@@ -97,8 +115,8 @@ class SingIn extends Component {
 
                 <TextField
                   onChange={this.handleChangeEventPass}
-                  error={this.state.password.length < 6 ? true : false}
-                  helperText={this.state.password.length < 6 ? 'Not enough characters' : ''}
+                  error={!this.isPasswordValid(password)}
+                  helperText={!this.isPasswordValid(password) ? this.state.passwordErrorMsg : ''}
                   InputProps={{
                     endAdornment: <InputAdornment onClick={() => this.setState({ showingPassword: !showingPassword })}>
                       {showingPassword
@@ -113,22 +131,30 @@ class SingIn extends Component {
                   variant="outlined" />
 
                 <FormControlLabel
-                  onClick={() => this.setState({ checkBoxChecked: !checkBoxChecked })}
+                  onClick={(e) => { e.preventDefault(); this.setState({ checkBoxChecked: !checkBoxChecked }) }}
                   style={this.space('m', 'b', 24)}
-                  control={<Checkbox color="primary" />}
+                  control={<Checkbox color="primary" checked={checkBoxChecked} />}
                   label="Accept the terms & conditions" />
 
               </form>
 
               <Button
-                disabled={checkBoxChecked ? false : true}  
+                onClick={() => {this.setState({ showSnackBar: true })}}
+                disabled={!isFormValid}
                 fullWidth={true}
                 variant="contained"
                 color="primary"
                 size='large'>
                 Sign In
               </Button>
-
+              <Snackbar
+                onClose={() => {this.setState({ showSnackBar: false })}}
+                open={this.state.showSnackBar}
+                autoHideDuration={3000}>
+                <Alert elevation={6} variant="filled" severity={showSnackBar ? "success" : "error"}>
+                  {showSnackBar ? 'You are Signed In !' : 'Incorrect inputs'}
+                </Alert>
+              </Snackbar>
             </Card>
 
           </Grid>
